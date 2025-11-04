5b52129138cd0779bd5b139683f9bd400f939850
krishnan1412 <167602903+krishnan1412@users.noreply.github.com>
2025-11-04 10:35:18 +0530

    Backend code

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello from Kubernetes!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
